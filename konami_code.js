f5bf9718df9215a3d190fee2f77770eadc0624c3
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-24 23:22:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,5 +4,7 @@ let index = 0;
 function init(e) {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.details || e.which)
-
+  if(key === code[index]){
+    index++;
+  }
 }
