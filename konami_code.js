d7818279448579345ea1bfed2ee52f06b7c99db2
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-24 23:35:16 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,22 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
- function onKeyDownHandler(e) {
-    //e.stopPropagation();
+
+  function onKeyDownHandler(e) {
+    e.stopPropagation();
     const key = parseInt(e.detail || e.which);
     if (code[index] === key){
-      index++;
+      ++index;
       if (index === code.length){
-        alert("Congrats!!");
+        alert("Hurray!");
         index = 0;
       }
     } else {
       index = 0;
     }
+  }
 
-const body = document.querySelector('body');
-body.addEventListener('keydown',onKeyDownHandler());
+  document.body.addEventListener('keydown', onKeyDownHandler);
+
 }
